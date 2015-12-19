742c687a4dffe50eb5e76cc24b77036d4a00fa2c
CSO <Chr.sorensen1990@gmail.com>
2015-12-19 16:31:30 +0100

    fox

--- fitness/routes/index.js
+++ fitness/routes/index.js
@@ -34,21 +34,15 @@ router.get('/login', function(req, res) {
     res.render('login', { user : req.user });
 });
 
-router.get('/exercises', isAuthenticated, function(req, res) {
+router.get('/exercises/:workdoutId', isAuthenticated, function(req, res) {
 	var workouts = mongoose.model('workouts');
+	var query  = workouts.findOne({ '_id': req.params.workdoutId});
 	
-    workouts.find().where('userName').equals(req.user.username).exec(function(err, data) {
+	query.exec(function(err, data) {
        //if (err) return console.error(err);
-	   var returnData;
-	   if(data.exercises){
-			for(var exercise in data.exercises){
-				if(exercise._id === data._id){
-					returnData = { 'exercises': exercise};
-					break;
-				}
-			}
-	   }
-       res.render('exercises', returnData);
+		var returnData = { 'workout': data};
+		console.log(returnData);
+		res.render('exercise', returnData);
     });
 });
 
